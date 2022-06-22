a7ec67efacc203780469c3632099dcdd8bd3afc9
Ethan Young <ethanyoung@hotmail.co.uk>
2022-06-22 15:54:48 +0100

    Refactored Code, added removal of images that do not have products in the csv

--- clean_images.py
+++ clean_images.py
@@ -1,7 +1,8 @@
 from PIL import Image
-import os
 import glob
 import numpy as np
+import pandas as pd
+import os
 
 def resize_image(final_size, im):
     size = im.size
@@ -12,19 +13,56 @@ def resize_image(final_size, im):
     new_im.paste(im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
     return new_im
 
+def remove_Unmatched_Images_ByID():
+    filelist = glob.glob('images/*.jpg')
+    id_list = []
+    for file in filelist:
+        str = file.replace('images/', '')
+        str = str.replace('.jpg', '')
+        id_list.append(str)
+
+    images_df = pd.read_csv("Images.csv", lineterminator='\n')
+
+    for idx, id in enumerate(id_list):
+        if id not in images_df['id'].unique():
+            os.remove('images/' + id + '.jpg')
+
 def save_Images_toNumpy():
     filelist = glob.glob('cleaned_images/*.jpg')
     temp = np.array([np.array(Image.open(fname)) for fname in filelist])
     np.save('images', temp)
 
-if __name__ == '__main__':
+def min_Dimensions_of_Image():
     path = "images/"
     dirs = os.listdir(path)
-    final_size = 512
+    min_Height = 999999
+    min_Width = 999999
+    for n, item in enumerate(dirs, 1):
+        im = Image.open('images/' + item)
+        h = im.size[0]
+        w = im.size[1]
+        if h < min_Height:
+            min_Height = h
+        if w < min_Width:
+            min_Width = w
+
+    print(f'The minimum Height was: {min_Height}')
+    print(f'The minimum Width was: {min_Width}')
+
+if __name__ == '__main__':
+    remove_Unmatched_Images_ByID()
+    #Find min Dimensions and then choose a valid FinalSize
+    min_Dimensions_of_Image()
+    path = "images/"
+    dirs = os.listdir(path)
+    #Choosing 256 as FinalSize after evaluating the Min dimensions
+    final_size = 256
     for n, item in enumerate(dirs, 1):
         im = Image.open('images/' + item)
         new_im = resize_image(final_size, im)
         new_im.save(f'cleaned_images/{n}_resized.jpg')
     save_Images_toNumpy()
+
+    
 
     
